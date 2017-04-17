6c7d1375ab851eb0255ee51b8fd45c59db48ef53
Tobias Hilka <tobias.hilka@nexusgroup.com>
2017-04-17 18:31:48 +0200

    first files

---
+++ index.ios.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import ShoppingList from './common/shoppinglist'
+import HandleListItems from './common/handlelistitems'
+
+export default class MyFirstReactNativeProject extends Component {
+  constructor() {
+    super();
+    shoppingListItems = [
+        {
+          name: 'bread',
+          quantity: 1
+        },
+        {
+          name: 'chocolate',
+          quantity: 1000
+        },
+        {
+          name: 'milk',
+          quantity: 3
+        }
+      ];
+    this.state = {
+      listItems : shoppingListItems
+    }
+  }
+
+  render() {
+      const handleAdd = () => {
+          this.state.listItems.push({
+                name: 'new',
+                quantity: 10
+            });
+          this.setState(this.state)
+          
+        }; 
+        const handleRemove = () => {
+          this.state.listItems.pop();
+          this.setState(this.state)
+          
+        }; 
+
+    return (
+      <View>
+        <ShoppingList items={shoppingListItems}/>
+        <HandleListItems items={shoppingListItems} handleAdd={handleAdd} handleRemove={handleRemove}/>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('MyFirstReactNativeProject', () => MyFirstReactNativeProject);
